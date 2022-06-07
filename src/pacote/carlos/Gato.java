993dffd7d9ffe67ea73b6292b305ef17a2cb4eb4
Carlos Filho <barra.carlosfilho@hotmail.com>
2022-06-07 12:47:26 -0300

    first commit

---
+++ src/pacote/carlos/Gato.java
@@ -0,0 +1,5 @@
+package pacote.carlos;
+
+public class Gato {
+
+}
---
+++ src/pacote/carlos/programajava.java
@@ -0,0 +1,32 @@
+package pacote.carlos;
+
+import pacote.carlos.model.gato;
+
+public class programajava {
+
+	public static void main(String[] args) {
+		
+		
+		Gato gato = new Gato ();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World! " + (a+b));*/
+		
+		
+		
+	}
+
+}
+class Livros {
+	
+	private String nome;
+	private String npag;
+	
+	
+}
